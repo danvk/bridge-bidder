dde014f7d6f0e97b1c18f1ca207bc436f8ca2c97
Dan Vanderkam <danvdk@gmail.com>
2018-01-03 18:40:45 -0500

    remove played cards from hand

--- test/basic-hearts-strategy_test.ts
+++ test/basic-hearts-strategy_test.ts
@@ -71,5 +71,8 @@ describe('basic hearts strategy', () => {
     expect(playTrick(hands, ['2C QC 6C KC', '5S 6S 3S JS'])).to.equal(
       'S:6D 3D 4D AD'  // lead low from D suit to the Ace
     );
+    expect(playTrick(hands, ['2C QC 6C KC', '5S 6S 3S JS', '6D 3D 4D AD'])).to.equal(
+      'E:3S 7S 4S 8S'  // lead a low spade to the 8.
+    );
   });
 });
